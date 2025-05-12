0dd4b4f747027e6eefc808944d903dae357079c0
AJ Caldwell <tomatrow@gmail.com>
2025-05-11 17:30:37 -0700

    Add come comments

--- src/AiderCoderClient.ts
+++ src/AiderCoderClient.ts
@@ -1,7 +1,9 @@
 export type ChatType = "ask" | "code" | "architect"
 
 export interface AiderCoderState {
+	/** filenames in the editable context */
 	abs_fnames: string[]
+	/** filenames in the readonly context */
 	abs_read_only_fnames: string[]
 }
 
