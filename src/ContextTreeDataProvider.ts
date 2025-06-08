786a1b10fe64ec8aacb318455f3e9dcbebe785dd
AJ Caldwell <tomatrow@gmail.com>
2025-06-08 09:19:01 -0700

    Simplify context tree We remove id managment and save a few lines this way

--- src/ContextTreeDataProvider.ts
+++ src/ContextTreeDataProvider.ts
@@ -7,110 +7,78 @@ export type ContextTreeNodeData =
 	| { type: "EDITABLE_LIST" }
 	| { type: "EDITABLE_FILE"; absoluteFilePath: string }
 
-export type ContextTreeNode = ContextTreeNodeData & { id: string; adjacencies: { id: string }[] }
-export type ContextTreeGraph = Record<string, ContextTreeNode>
+export type ContextTreeNode = ContextTreeNodeData & { children: ContextTreeNode[] }
 
-function createContextTreeGraph(coder: AiderCoderState): ContextTreeGraph {
-	const nodes: ContextTreeNode[] = []
-
-	const root: ContextTreeNode = { id: "root", type: "ROOT", adjacencies: [] }
-	nodes.push(root)
+function createContextTree(coder: AiderCoderState): ContextTreeNode {
+	const children: ContextTreeNode[] = []
 
 	if (coder.abs_fnames.length) {
-		const editableFileNodes = coder.abs_fnames.map(
-			(absoluteFilePath): ContextTreeNode => ({
-				id: "EDITABLE_FILE_" + absoluteFilePath,
-				type: "EDITABLE_FILE",
-				adjacencies: [],
-				absoluteFilePath
-			})
-		)
+		const editableFileNodes: ContextTreeNode[] = coder.abs_fnames.map(absoluteFilePath => ({
+			type: "EDITABLE_FILE",
+			absoluteFilePath,
+			children: []
+		}))
 
-		const editableListNode = {
-			id: "editableList",
+		const editableListNode: ContextTreeNode = {
 			type: "EDITABLE_LIST",
-			adjacencies: editableFileNodes.map(node => ({ id: node.id }))
-		} satisfies ContextTreeNode
+			children: editableFileNodes
+		}
 
-		root.adjacencies.push({ id: editableListNode.id })
-
-		nodes.push(editableListNode, ...editableFileNodes)
+		children.push(editableListNode)
 	}
 
 	if (coder.abs_read_only_fnames.length) {
-		const readonlyFileNodes = coder.abs_read_only_fnames.map(
-			(absoluteFilePath): ContextTreeNode => ({
-				id: "READONLY_FILE" + absoluteFilePath,
+		const readonlyFileNodes: ContextTreeNode[] = coder.abs_read_only_fnames.map(
+			absoluteFilePath => ({
 				type: "READONLY_FILE",
-				adjacencies: [],
-				absoluteFilePath
+				absoluteFilePath,
+				children: []
 			})
 		)
 
-		const readonlyListNode = {
-			id: "readonlyList",
+		const readonlyListNode: ContextTreeNode = {
 			type: "READONLY_LIST",
-			adjacencies: readonlyFileNodes.map(node => ({ id: node.id }))
-		} satisfies ContextTreeNode
+			children: readonlyFileNodes
+		}
 
-		root.adjacencies.push({ id: readonlyListNode.id })
-
-		nodes.push(readonlyListNode, ...readonlyFileNodes)
+		children.push(readonlyListNode)
 	}
 
-	return Object.fromEntries(nodes.map(node => [node.id, node]))
+	return { type: "ROOT", children }
 }
 
 export class ContextTreeDataProvider implements TreeDataProvider<ContextTreeNode> {
-	graph: ContextTreeGraph = {}
+	rootNode: ContextTreeNode = { type: "ROOT", children: [] }
 
 	update(coder?: AiderCoderState) {
-		this.graph = coder ? createContextTreeGraph(coder) : {}
+		this.rootNode = coder ? createContextTree(coder) : { type: "ROOT", children: [] }
 	}
 
 	getChildren(element: ContextTreeNode | null): ContextTreeNode[] {
-		if (!element)
-			return [this.graph.editableList, this.graph.readonlyList].filter(
-				node => node !== undefined
-			)
-		return (
-			element.adjacencies
-				.map(adjacency => this.graph[adjacency.id])
-				.filter(node => node !== undefined) ?? []
-		)
+		if (!element) return this.rootNode.children
+		return element.children
 	}
 
 	getTreeItem(element: ContextTreeNode) {
 		switch (element.type) {
 			case "ROOT":
 				const treeItem = new TreeItem("Root", TreeItemCollapsibleState.Expanded)
-				treeItem.identifier = element.id
 				return treeItem
 			case "EDITABLE_LIST": {
 				const treeItem = new TreeItem("Editable", TreeItemCollapsibleState.Expanded)
-				treeItem.identifier = element.id
 				return treeItem
 			}
 			case "READONLY_LIST": {
 				const treeItem = new TreeItem("Readonly", TreeItemCollapsibleState.Expanded)
-				treeItem.identifier = element.id
 				return treeItem
 			}
+			case "EDITABLE_FILE":
 			case "READONLY_FILE": {
 				const treeItem = new TreeItem(
-					element.absoluteFilePath.replace(nova.workspace.path!, ""),
+					nova.path.basename(element.absoluteFilePath),
 					TreeItemCollapsibleState.None
 				)
-				treeItem.identifier = element.id
-				treeItem.path = element.absoluteFilePath
-				return treeItem
-			}
-			case "EDITABLE_FILE": {
-				const treeItem = new TreeItem(
-					element.absoluteFilePath.replace(nova.workspace.path!, ""),
-					TreeItemCollapsibleState.None
-				)
-				treeItem.identifier = element.id
+				treeItem.tooltip = nova.workspace.relativizePath(element.absoluteFilePath)
 				treeItem.path = element.absoluteFilePath
 				return treeItem
 			}
